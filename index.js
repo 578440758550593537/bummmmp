750edecd3db1b83f9bc305e0e60dae5a9a8a9798
Dark <51209105+578440758550593537@users.noreply.github.com>
2020-10-03 19:55:00 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = new require("discord.js");
+const bot = new Discord.Client();
+const config = new require("./config.json");
+const TOKEN = config.token;
+
+bot.login(TOKEN);
+
+bot.on("message", function(message)
+{
+
+    if (message.content == 'ab!start'){
+
+      message.channel.send("**Auto-bumping started! Next message in 20 minutes from now!**")  
+
+      var interval = setInterval (function ()
+            {
+              message.channel.send("s!up")
+            }, 14400000);
+
+      }
+
+});
